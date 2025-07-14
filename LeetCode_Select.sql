8d8721a231b463f551a187875ff2c58a114f533e
Gulshan Kumar <145855336+gulshan766750@users.noreply.github.com>
2025-07-14 21:24:14 +0530

    Update LeetCode_Select.sql

--- LeetCode_Select.sql
+++ LeetCode_Select.sql
@@ -1,1 +1,30 @@
+-- 1757. Recyclable and Low Fat Products
+# Write your MySQL query statement below
+select product_id
+from Products
+where low_fats = 'Y'
+and recyclable = 'Y'
 
+-- 584. Find Customer Referee
+
+select name
+from Customer
+where referee_id != 2
+OR referee_id is null
+
+-- 595. Big Countries
+select name, population,area
+from World
+where area >=3000000
+or population >= 25000000
+
+-- 1148. Article Views I
+select distinct author_id as id
+from views
+where author_id = viewer_id
+order by author_id
+
+-- 1683. Invalid Tweets
+select tweet_id
+from Tweets
+where length(content) > 15
